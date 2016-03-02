f17d7ea48aecce7457448ceb34776e5e43f1afa6
Andreas Drivenes <andreas.drivenes@gmail.com>
2016-03-02 10:40:59 +0100

    Add main controller and Construction REST factory
    Also remove unused dependencies and add Angular UI Bootstrap

--- static/javascripts/app.js
+++ static/javascripts/app.js
@@ -2,6 +2,8 @@
 angular
   .module('idrettsanlegg', [
     'ngRoute',
+    'ngResource',
+    'ui.bootstrap'
   ])
   .config(function ($routeProvider, $locationProvider) {
     $locationProvider
@@ -9,9 +11,32 @@ angular
       .hashPrefix('!');
     $routeProvider
       .when('/', {
+        controller: 'MainController',
         templateUrl: 'static/templates/main.html'
       })
       .otherwise({
         redirectTo: '/'
       });
+  })
+  // This is the controller for the main route,
+  // the scope controls what is visible in the template
+  .controller('MainController', function($scope, Construction) {
+    Construction.query(function(data) {
+      //$scope.constructions = data.objects etc.
+      console.log(data);
+    });
+    $scope.constructions = ['1', '2', '3'];
+
+  })
+  // Interface with the REST API, inject Construction and use
+  // .query, .get, .delete etc.
+  .factory('Construction', function ($resource) {
+    return $resource('http://127.0.0.1:8000/api/v1/Idrettsanlegg/?format=json', { id: '@_id' }, {
+      update: {
+        method: 'PUT'
+      },
+      query: {
+        method: 'GET'
+      }
+    });
   });
